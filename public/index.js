571241eb58a34a7b0218188be200457140014061
fr0stieee <ben_angel_1@yahoo.com.au>
2020-08-25 15:37:40 +1000

    Formatting, added poke types

--- public/index.js
+++ public/index.js
@@ -1,127 +1,124 @@
-const poke_container = document.getElementById('poke_container');
+const poke_container = document.getElementById("poke_container");
 const pokemons_number = 35;
 const colors = {
-	normal: '#a8a878',
-	fire: '#f08030',
-	fighting: '#c03028',
-	water: '#6890f0',
-	flying: '#a890f0',
-	grass: '#78c850',
-	poison: '#a040a0',
-	electric: '#f8d030',
-	ground: '#e0c068',
-	psychic: '#f85888',
-	rock: '#b8a038',
-	ice: '#98d8d8',
-	bug: '#a8b820',
-	dragon: '#7038f8',
-	ghost: '#705898',
-	dark:'#705848',
-	steel: '#b8b8d0',
-	fairy: '#ee99ac',
-	'???': '#68a090'
+  normal: "#a8a878",
+  fire: "#f08030",
+  fighting: "#c03028",
+  water: "#6890f0",
+  flying: "#a890f0",
+  grass: "#78c850",
+  poison: "#a040a0",
+  electric: "#f8d030",
+  ground: "#e0c068",
+  psychic: "#f85888",
+  rock: "#b8a038",
+  ice: "#98d8d8",
+  bug: "#a8b820",
+  dragon: "#7038f8",
+  ghost: "#705898",
+  dark: "#705848",
+  steel: "#b8b8d0",
+  fairy: "#ee99ac",
+  "???": "#68a090",
 };
 const main_types = Object.keys(colors);
 
 const fetchPokemons = async () => {
-	for (let i = 1; i <= pokemons_number; i++) {
-		await getPokemon(i);
-	}
+  for (let i = 1; i <= pokemons_number; i++) {
+    await getPokemon(i);
+  }
 };
 
-const getPokemon = async id => {
-	const url = `http://localhost:3000/api/v1/pokemon/${id}`;
-	const res = await fetch(url);
-	const pokemon = await res.json();
-	createPokemonCard(pokemon);
+const getPokemon = async (id) => {
+  const url = `http://localhost:3000/api/v1/pokemon/${id}`;
+  const res = await fetch(url);
+  const pokemon = await res.json();
+  createPokemonCard(pokemon);
 };
 
 function createPokemonCard(pokemon) {
-	const pokemonEl = document.createElement('div');
-	pokemonEl.classList.add('pokemon');
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("pokemon");
 
-	let pokemonId = pokemon.id;
-	// const poke_types = pokemon.types.map(type => type.type.name);
-	const type = pokemon.types[0].type.name;
-	let type2 = '';
-	if(pokemon.types[1] != undefined){
-		type2 = pokemon.types[1].type.name;
-	}
-	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-	let color;
-	if(type == 'normal' && type2 != undefined){
-		color = colors[type2];
-	}else{
-		color = colors[type];
-	}
-	pokemonEl.style.backgroundColor = color;
+  let pokemonId = pokemon.id;
+  // const poke_types = pokemon.types.map(type => type.type.name);
+  const type = pokemon.types[0].type.name;
+  let type2 = "";
+  if (pokemon.types[1] != undefined) {
+    type2 = pokemon.types[1].type.name;
+  }
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  let color;
+  if (type == "normal" && type2 != undefined) {
+    color = colors[type2];
+  } else {
+    color = colors[type];
+  }
+  pokemonEl.style.backgroundColor = color;
 
-	if(pokemonId < 10){
-		pokemonId = `00${pokemonId}`.toString();
-	}
-	else if(pokemonId < 100){
-		pokemonId = `0${pokemonId}`.toString();
-	}
+  if (pokemonId < 10) {
+    pokemonId = `00${pokemonId}`.toString();
+  } else if (pokemonId < 100) {
+    pokemonId = `0${pokemonId}`.toString();
+  }
 
-	if(type2 !=''){
-		type2 = `/${type2}`;
-	}
-	const pokeInnerHTML = `
+  if (type2 != "") {
+    type2 = `/${type2}`;
+  }
+  const pokeInnerHTML = `
         <div class="img-container"><img src="https://pokeres.bastionbot.org/images/pokemon/${
-			pokemon.id
-		}.png" alt="${name}" />
+          pokemon.id
+        }.png" alt="${name}" />
         </div>
         <div class="info">
             <span class="number">#${pokemon.id
-							.toString()
-							.padStart(3, '0')}</span>
+              .toString()
+              .padStart(3, "0")}</span>
             <h3 class="name">${name}</h3>
             <small class="type">Type: <span>${type}${type2}</span></small>
         </div>
     `;
 
-	pokemonEl.innerHTML = pokeInnerHTML;
+  pokemonEl.innerHTML = pokeInnerHTML;
 
-	poke_container.appendChild(pokemonEl);
+  poke_container.appendChild(pokemonEl);
 }
 
-const searchBar = document.getElementById('searchTerm');
-searchBar.addEventListener('keyup', e => {
-    // console.log(searchBar.innerText);
-	// console.log(searchBar.value);
-	console.log(searchBar.value);
-	const pokemonCards = document.getElementsByClassName('name');
-	let searchVal = new RegExp('(' + searchBar.value + ')+','img');
-	console.log(searchVal);
-	console.log(searchVal.test('Bulbasaur')+ "Bulbasaur");
-	console.log(searchVal.test('Ivysaur') + "Ivysaur");
-	console.log(searchVal.test('Charmander')+ "Charmander");
-	console.log(searchVal.test('Ekans') + "Ekans");
-	console.log(searchVal.test('Rayquaza')+ "Rayquaza");
-	console.log(searchVal.test('Benjamin') + "Benjamin");
-	
-	for (let i = 0; i <= pokemonCards.length; i++) {
+const searchBar = document.getElementById("searchTerm");
+searchBar.addEventListener("keyup", (e) => {
+  // console.log(searchBar.innerText);
+  // console.log(searchBar.value);
+  console.log(searchBar.value);
+  const pokemonCards = document.getElementsByClassName("name");
+  let searchVal = new RegExp("(" + searchBar.value + ")+", "img");
+  console.log(searchVal);
+  console.log(searchVal.test("Bulbasaur") + "Bulbasaur");
+  console.log(searchVal.test("Ivysaur") + "Ivysaur");
+  console.log(searchVal.test("Charmander") + "Charmander");
+  console.log(searchVal.test("Ekans") + "Ekans");
+  console.log(searchVal.test("Rayquaza") + "Rayquaza");
+  console.log(searchVal.test("Benjamin") + "Benjamin");
 
-		//if(searchVal.)
-		// if(pokemonCards.item(i).innerHTML == searchVal){
-		// 	console.log("Found it");
-		// 	console.log(pokemonCards.item(i).innerHTML);
-		// 	break;
-		// }
-		// else{
-		// 	console.log("Didn't find it");
-		// }
-	}
+  for (let i = 0; i <= pokemonCards.length; i++) {
+    //if(searchVal.)
+    // if(pokemonCards.item(i).innerHTML == searchVal){
+    // 	console.log("Found it");
+    // 	console.log(pokemonCards.item(i).innerHTML);
+    // 	break;
+    // }
+    // else{
+    // 	console.log("Didn't find it");
+    // }
+  }
 
-	// if(searchBar.value == pokemonCards.innerHTML)
-	// {
-	// 	console.log(pokemonCards.value);
-	// }
-	// else{
-	// 	console.log(searchBar.value);
-	// 	console.log('false', pokemonCards);
-	// }
-})
-
+  // if(searchBar.value == pokemonCards.innerHTML)
+  // {
+  // 	console.log(pokemonCards.value);
+  // }
+  // else{
+  // 	console.log(searchBar.value);
+  // 	console.log('false', pokemonCards);
+  // }
+});
 
 fetchPokemons();
--- src/api/MeasureAPI.js
+++ src/api/MeasureAPI.js
@@ -1,41 +1,20 @@
 const express = require("express");
 const axios = require("axios");
 const router = express.Router();
-const ip = require('ip');
+const ip = require("ip");
 
 console.dir(ip.address());
 const apiKey = process.env.MEASUREAPI;
-const BASE_URL = `http://mot-api.bluebulbprojects.com/v1/units/weight/`;
+const BASE_URL = `http://mot-api.bluebulbprojects.com/v1/testitems/search?measure=weight&unit=lbs&amount=100&key=${apiKey}`;
 
-router.get("/", (req, res) => {
-  //   try {
-  //     //   const params = new URLSearchParams({
-  //     //     measure: 'weight',
-  //     //     unit: 'kgms',
-  //     //     amount: 100,
-  //     //     key: apiKey
-  //     //   });
-  //     // res.json(work);
-  //     const { data } = await axios.get(`${BASE_URL}${query}`);
-
-  //     res.json(data);
-  //   } catch (error) {
-  //     res.json(error);
-  //     next(error);
-  //   }
-
-    //   const { data } = axios.get(`${BASE_URL}${query}`).then(function (response) {
-    //     // handle success
-    //     console.log(response);
-    //   })
-    //   .catch(function (error) {
-    //     // handle error
-    //     console.log(error);
-    //   })
-    //   console.log(data);
-    res.json({
-        message: "End points not working"
-    })
+router.get("/", async (req, res, next) => {
+  console.log(BASE_URL);
+  try {
+    const { data } = await axios.get(BASE_URL);
+    return res.json(data);
+  } catch (error) {
+    return next(error);
+  }
 });
 
 module.exports = router;
