0c04f7701486b4c94b492e1993e9895140523d5a
fr0stieee <ben_angel_1@yahoo.com.au>
2020-08-25 13:18:57 +1000

    Caching data + front end changes

--- public/index.js
+++ public/index.js
@@ -1,20 +1,25 @@
 const poke_container = document.getElementById('poke_container');
-const pokemons_number = 20;
+const pokemons_number = 80;
 const colors = {
-	fire: '#FDDFDF',
-	grass: '#DEFDE0',
-	electric: '#FCF7DE',
-	water: '#DEF3FD',
-	ground: '#f4e7da',
-	rock: '#d5d5d4',
-	fairy: '#fceaff',
-	poison: '#98d7a5',
-	bug: '#f8d5a3',
-	dragon: '#97b3e6',
-	psychic: '#eaeda1',
-	flying: '#F5F5F5',
-	fighting: '#E6E0D4',
-	normal: '#F5F5F5'
+	normal: '#a8a878',
+	fire: '#f08030',
+	fighting: '#c03028',
+	water: '#6890f0',
+	flying: '#a890f0',
+	grass: '#78c850',
+	poison: '#a040a0',
+	electric: '#f8d030',
+	ground: '#e0c068',
+	psychic: '#f85888',
+	rock: '#b8a038',
+	ice: '#98d8d8',
+	bug: '#a8b820',
+	dragon: '#7038f8',
+	ghost: '#705898',
+	dark:'#705848',
+	steel: '#b8b8d0',
+	fairy: '#ee99ac',
+	'???': '#68a090'
 };
 const main_types = Object.keys(colors);
 
@@ -25,7 +30,7 @@ const fetchPokemons = async () => {
 };
 
 const getPokemon = async id => {
-	const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+	const url = `http://localhost:3000/api/v1/pokemon/${id}`;
 	const res = await fetch(url);
 	const pokemon = await res.json();
 	createPokemonCard(pokemon);
@@ -35,6 +40,7 @@ function createPokemonCard(pokemon) {
 	const pokemonEl = document.createElement('div');
 	pokemonEl.classList.add('pokemon');
 
+	let pokemonId = pokemon.id;
 	const poke_types = pokemon.types.map(type => type.type.name);
 	const type = main_types.find(type => poke_types.indexOf(type) > -1);
 	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
@@ -42,11 +48,16 @@ function createPokemonCard(pokemon) {
 	
 	pokemonEl.style.backgroundColor = color;
 
+	if(pokemonId < 10){
+		pokemonId = `00${pokemonId}`.toString();
+	}
+	else if(pokemonId < 100){
+		pokemonId = `0${pokemonId}`.toString();
+	}
 	const pokeInnerHTML = `
-        <div class="img-container">
-            <img src="https://pokeres.bastionbot.org/images/pokemon/${
-							pokemon.id
-						}.png" alt="${name}" />
+        <div class="img-container"><img src="https://pokeres.bastionbot.org/images/pokemon/${
+			pokemon.id
+		}.png" alt="${name}" />
         </div>
         <div class="info">
             <span class="number">#${pokemon.id
--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -1,18 +1,71 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 
 const router = express.Router();
 
-const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/1';
-router.get('/', async (req, res, next) => {
-    try{
-        const {data} = await axios.get(BASE_URL);
-        res.json(data);
-
-    }catch(error){
-        next(error);
-
-    }
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+let cacheData = [];
+let cacheTime;
+router.get("/", async (req, res, next) => {
+  try {
+      let query = '?limit=1048'
+    const { data } = await axios.get(BASE_URL+query);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
 });
+router.get("/cache", (req, res) => {
+  try {
+    return res.json(cacheData);
+  } catch (error) {
+    return res.json(error);
+  }
+});
+router.get("/:id", async (req, res, next) => {
+  try {
+    let query = req.params.id;
+    let foundData = false;
+    if (cacheData.length >= 1) {
+      console.log("in if " + cacheData.length);
+      for (let i = 0; i < cacheData.length; i++) {
+        if (query == cacheData[i].id) {
+          foundData = true;
+          return res.json(cacheData[i]);
+        } else {
+          foundData = false;
+        }
+      }
+      if (!foundData) {
+        const { data } = await axios.get(BASE_URL + query);
+        cacheData.push(data);
+        return res.json(data);
+      }
+    } else {
+      console.log("in else");
+      const { data } = await axios.get(BASE_URL + query);
+
+      cacheData.push(data);
+      console.log(cacheData.length);
+      return res.json(data);
+    }
+  } catch (error) {
+    return next(error);
+  }
+  //   res.end();
+});
+// router.get('/limit/:id', async(req,res,next) =>{
+//     try {
+//         const limitURL = 'https://pokeapi.co/api/v2/pokemon';
+//         let query = `?limit=${req.params.id}`;
+//         console.log(limitURL+query);
+//         const{data} = await axios.get(limitURL+query);
+//         res.json(data);
+
+//     } catch (error) {
+//         res.json(error);
+
+//     }
+// })
 
 module.exports = router;
